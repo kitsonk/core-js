b415201aaf3241f5c2896194c4628de274a75172
Kitson Kelly <dojo@kitsonkelly.com>
2014-07-12 10:27:23 +0100

    Add Gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+	grunt.loadNpmTasks('intern');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		intern: {
+			remote: {
+				options: {
+					runtType: 'runner',
+					config: 'tests/config',
+					reporters: [ 'runner' ]
+				}
+			},
+			node: {
+				options: {
+					runType: 'client',
+					config: 'tests/config',
+					reporters: [ 'console' ]
+				}
+			}
+		}
+	});
+
+	grunt.registerTask('test', function (target) {
+		if (!target) {
+			target = 'node';
+		}
+
+		function addReporter(reporter) {
+			var property = 'intern.' + target + '.options.reporters',
+				value = grunt.config.get(property);
+
+			if (value.indexOf(reporter) !== -1) {
+				return;
+			}
+
+			value.push(reporter);
+			grunt.config.set(property, value);
+		}
+
+		if (this.flags.coverage) {
+			addReporter('lcovhtml');
+		}
+		if (this.flags.console) {
+			addReporter('console');
+		}
+
+		grunt.task.run('intern:' + target);
+	});
+};

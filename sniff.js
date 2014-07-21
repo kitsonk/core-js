80e208f09f421445fda3b50202c0923e8eb68ebd
Kitson Kelly <dojo@kitsonkelly.com>
2014-07-21 07:07:00 +0100

    Add IE 11+ feature detection.

--- sniff.js
+++ sniff.js
@@ -23,9 +23,9 @@ define([
 		has.add('khtml', dav.indexOf('Konqueror') >= 0 ? tv : undefined);
 		has.add('webkit', parseFloat(dua.split('WebKit/')[1]) || undefined);
 		has.add('chrome', parseFloat(dua.split('Chrome/')[1]) || undefined);
-		has.add('safari', dav.indexOf('Safari') >= 0 && !has('chrome') ? parseFloat(dav.split('Version/')[1]) : undefined);
+		has.add('safari', dav.indexOf('Safari') >= 0 && !has('chrome') ? parseFloat(dav.split('Version/')[1])
+			: undefined);
 		has.add('mac', dav.indexOf('Macintosh') >= 0);
-		has.add('quirks', document.compatMode === 'BackCompat');
 		if (dua.match(/(iPhone|iPod|iPad)/)) {
 			var p = RegExp.$1.replace(/P/, 'p');
 			var v = dua.match(/OS ([\d_]+)/) ? RegExp.$1 : '1';
@@ -34,20 +34,24 @@ define([
 			has.add('ios', os);
 		}
 		has.add('android', parseFloat(dua.split('Android ')[1]) || undefined);
-		has.add('bb', (dua.indexOf('BlackBerry') >= 0 || dua.indexOf('BB10') >= 0) && parseFloat(dua.split('Version/')[1]) || undefined);
+		has.add('bb', (dua.indexOf('BlackBerry') >= 0 || dua.indexOf('BB10') >= 0)
+			&& parseFloat(dua.split('Version/')[1]) || undefined);
+		// Used for detecting IE 11 +
+		has.add('trident', parseFloat(dav.split('Trident/')[1]) || undefined);
 
 		has.add('svg', typeof SVGAngle !== 'undefined');
 
 		if (!has('webkit')) {
 			// Opera
 			if (dua.indexOf('Opera') >= 0) {
-				// see http://dev.opera.com/articles/view/opera-ua-string-changes and http://www.useragentstring.com/pages/Opera/
+				// see http://dev.opera.com/articles/view/opera-ua-string-changes and
+				// http://www.useragentstring.com/pages/Opera/
 				// 9.8 has both styles; <9.8, 9.9 only old style
 				has.add('opera', tv >= 9.8 ? parseFloat(dua.split('Version/')[1]) || tv : tv);
 			}
 
 			// Mozilla and firefox
-			if (dua.indexOf('Gecko') >= 0 && !has('khtml') && !has('webkit')) {
+			if (dua.indexOf('Gecko') >= 0 && !has('khtml') && !has('webkit') && !has('trident')) {
 				has.add('mozilla', tv);
 			}
 			if (has('mozilla')) {

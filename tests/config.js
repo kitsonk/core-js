890c215e7a8190b3c8195f6b8003f090110fe927
Kitson Kelly <dojo@kitsonkelly.com>
2014-08-30 08:07:04 +0100

    Update browser versions.

--- tests/config.js
+++ tests/config.js
@@ -11,13 +11,13 @@ define({
 	},
 
 	environments: [
-		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
-		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
+		{ browserName: 'internet explorer', version: '11', platform: 'Windows 7' },
+		{ browserName: 'internet explorer', version: '10', platform: 'Windows 7' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'chrome', version: '35', platform: [ 'Windows 7', 'Windows XP', 'Linux', 'OS X 10.8' ] },
-		{ browserName: 'firefox', version: '', platform: [ 'Windows 7', 'Windows XP', 'Linux', 'OS X 10.8' ] },
-		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }
-		// { browserName: 'safari', version: '7', platform: 'OS X 10.9' }
+		{ browserName: 'chrome', version: '', platform: [ 'Windows 7', 'Windows XP', 'Linux', 'OS X 10.8' ] },
+		{ browserName: 'firefox', version: '', platform: [ 'Windows 7', 'Windows XP', 'Linux', 'OS X 10.9' ] },
+		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
+		{ browserName: 'safari', version: '7', platform: 'OS X 10.9' }
 	],
 
 	maxConcurrency: 3,

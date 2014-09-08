ba4b30af19ab608e0a35345f87185ee37c0d9d51
Kitson Kelly <dojo@kitsonkelly.com>
2014-09-08 22:00:46 +0100

    Removing OSX 10.9 due to SauceLab issues.

--- tests/config.js
+++ tests/config.js
@@ -15,9 +15,9 @@ define({
 		{ browserName: 'internet explorer', version: '10', platform: 'Windows 7' },
 		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
 		{ browserName: 'chrome', version: '', platform: [ 'Windows 7', 'Windows XP', 'Linux', 'OS X 10.8' ] },
-		{ browserName: 'firefox', version: '', platform: [ 'Windows 7', 'Windows XP', 'Linux', 'OS X 10.9' ] },
-		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
-		{ browserName: 'safari', version: '7', platform: 'OS X 10.9' }
+		{ browserName: 'firefox', version: '', platform: [ 'Windows 7', 'Windows XP', 'Linux', 'OS X 10.8' ] },
+		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }
+		// { browserName: 'safari', version: '7', platform: 'OS X 10.9' }
 	],
 
 	maxConcurrency: 3,
